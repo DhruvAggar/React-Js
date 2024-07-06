5beb889b28d5f9718b9f4735f04dbe1aa7932e9f
Dhruv Aggarwal <96526382+DhruvAggar@users.noreply.github.com>
2024-07-06 13:30:59 +0530

    Add files via upload

---
+++ react-props-practice/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./caard";
+
+function App() {
+  return (
+    <div>
+      <h1>My Contacts</h1>
+      <Card
+        name="Beyonce"
+        img="https://imgs.search.brave.com/I-UtvHlxt18VJK0iYAmF-nvJCyPJoZkwrvQX36iigBU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdDIu/ZGVwb3NpdHBob3Rv/cy5jb20vNTMyNjMz/OC85MjQ2L2kvNDUw/L2RlcG9zaXRwaG90/b3NfOTI0NjA2MDAt/c3RvY2stcGhvdG8t/c2luZ2VyLWJleW9u/Y2Uta25vd2xlcy5q/cGc"
+        phone="9911463666"
+        email="dhruvagg.d@gmail.com"
+      />
+    </div>
+  );
+}
+
+export default App;
